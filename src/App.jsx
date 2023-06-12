c1e847eb86e50069c9669ad94f74023e123f9455
Muhammad Nur Azis Mujiono <63378909+nurazis@users.noreply.github.com>
2023-06-12 16:06:22 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col">
+      <p className="m-auto text-2xl text-emerald-500 font-semibold">Hello</p>
+    </div>
+  );
+}
+
+export default App;
